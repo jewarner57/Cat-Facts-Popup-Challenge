721a5ad037e44fd2fda544905f3ee2a864351dfb
Jonathan Warner <jewarner57@gmail.com>
2021-12-30 15:54:08 -0500

    feat: popup should not display on reload if user clicks the x multiple times

--- popup.js
+++ popup.js
@@ -1,3 +1,17 @@
+const popup = document.querySelector('.cat-popup')
+
+// Get saved state from local storage so we dont display if the user
+// previously double clicked the x
+const shouldDisplay = localStorage.getItem('shouldDisplay') ? JSON.parse(localStorage.getItem('shouldDisplay')) : 'true';
+console.log(shouldDisplay)
+
+// wait 3 seconds before showing popup
+setTimeout(() => {
+  if (shouldDisplay === 'true') {
+    popup.classList.add('showPopup')
+  }
+}, 3000)
+
 // should display cat facts
 document.getElementById('get-facts').addEventListener('click', async (e) => {
   const facts_elem = document.getElementById('cat-facts')
@@ -22,5 +36,17 @@ async function getFacts(factCount) {
 }
 
 // should slide down on x click
+const xButton = document.getElementById('close-popup')
+// hide popup on click
+xButton.addEventListener('click', e => hidePopup())
+// permahide on doubleclick
+xButton.addEventListener('dblclick', () => {
+  hidePopup()
+  // Save shouldDisplay as false so the user doesn't see popup again
+  localStorage.setItem('shouldDisplay', JSON.stringify('false'));
+});
 
-// should not display again if user clicks x twice or more
+function hidePopup() {
+  popup.classList.remove('showPopup')
+  popup.classList.add('hidePopup')
+}
