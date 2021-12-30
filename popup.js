c5c09f386916d2dcdf8e3fb71f06219cca5657bb
Jonathan Warner <jewarner57@gmail.com>
2021-12-30 15:34:04 -0500

    feat: get facts from cat api

--- popup.js
+++ popup.js
@@ -1,24 +1,26 @@
 // should display cat facts
 document.getElementById('get-facts').addEventListener('click', async (e) => {
-  const facts = document.getElementById('cat-facts')
+  const facts_elem = document.getElementById('cat-facts')
+  // remove existing cat facts
+  facts_elem.innerHTML = ""
+
   const factCount = 5
   const factList = await getFacts(factCount)
-  console.log(factList)
 
-  for (let i = 0; i < factCount; i++) {
-
+  for (let fact of factList) {
+    const child = document.createElement('p');
+    child.innerHTML = fact.text
+    facts_elem.appendChild(child)
   }
 
 })
 
-
 async function getFacts(factCount) {
-  const res = await fetch(`https://cat-fact.herokuapp.com/facts?amount=${factCount}`)
+  const res = await fetch(`https://cat-fact.herokuapp.com/facts/random?amount=${factCount}`)
   const json = await res.json()
-
   return json
 }
 
-// should slide down on x double click
+// should slide down on x click
 
 // should not display again if user clicks x twice or more
